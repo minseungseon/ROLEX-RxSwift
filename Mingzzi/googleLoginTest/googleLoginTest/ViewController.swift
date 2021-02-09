c040dd1ba2bd461710bdda05a5d5614fb1e067fe
xwoud <xwoud@naver.com>
2021-02-09 18:23:21 +0900

    [upload] googleLogin 프로젝트 생성

---
+++ Mingzzi/googleLoginTest/googleLoginTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  googleLoginTest
+//
+//  Created by 김민희 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
